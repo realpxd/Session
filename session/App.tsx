a2d95765d7ea5686e8b31c44ee47ca5515bc0a2a
Hanish Kumar <142662543+toxiic-me@users.noreply.github.com>
2024-02-06 01:11:54 +0530

    Update App.tsx

--- session/App.tsx
+++ session/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -9,28 +9,30 @@ import {
   View,
   Button
 } from 'react-native';
-import { useContext } from 'react';
-import { RootContext } from './RootContext';
+import RootContext  from './RootContext';
 import { getGlobalStyles } from './globalStyles';
 import MainActivity from './components/MainActivity/MainActivity';
 import NavActivity from './components/UserActivity/NavActivity';
 
 const styles = getGlobalStyles();
+const appContext = createContext(1)
 
 const App = () => {
   // const text = useContext(RootContext);
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [globalState, setGlobalState] = useState(1)
 
   return (
-    <>
+      <appContext.Provider value={[globalState, setGlobalState]}>
       {!isUserLoggedIn ?
           <MainActivity styles={styles} />
         :
           <NavActivity styles={styles} />
       }
-    </>
+    </appContext.Provider>
   );
 }
 
 
 export default App;
+export { AppContext }

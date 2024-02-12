f1ec8d278da63bc112df48cce42fd9f645941411
ProgrammerXD <thisisnamansaini@gmail.com>
2024-02-12 23:18:05 +0530

    open login panel by default

--- session/App.tsx
+++ session/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
   // const { warehouse, setWarehouse } = useContext(AppContext);
 
   // console.log('App.tsx: warehouse: ', warehouse);
-  let isUserLoggedIn = true;
+  let isUserLoggedIn = false;
 
   return (
     <>
